c87132297a6e24410d2506048c2a7ef872d1d684
kyle-air <kylekwon.dev@gmail.com>
2026-06-19 18:19:21 +0900

    learn GeometryReader

---
+++ SwiftUIPractice/HelloSwiftUI/HelloSwiftUI/GeometryReader/GeometryReaderView.swift
@@ -0,0 +1,41 @@
+//
+//  GeometryReaderView.swift
+//  HelloSwiftUI
+//
+//  Created by KWON HYUKMIN on 6/19/26.
+//
+
+import SwiftUI
+
+struct GeometryReaderView: View {
+    var body: some View {
+//        GeometryReader { _ in 
+//            Circle()
+//                .fill(.purple)
+//                .frame(width: 200, height: 200)
+//                .overlay(Text("Center").font(.title))
+//        }.background(.gray)
+        
+
+        
+//        GeometryReader { _ in
+//            Circle()
+//                .fill(.blue)
+//                .frame(width: 350, height: 350)
+//            
+//            Circle()
+//                .fill(.orange)
+//                .frame(width: 280, height: 280)
+//            
+//            Circle()
+//                .fill(.purple)
+//                .frame(width: 200, height: 200)
+//            Text("Top Leading")
+//                .font(.largeTitle)
+//        }.background(.gray)
+    }
+}
+
+#Preview {
+    GeometryReaderView()
+}
